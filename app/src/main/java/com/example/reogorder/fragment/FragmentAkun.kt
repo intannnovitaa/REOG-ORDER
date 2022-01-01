c416e12fe50f753ff698270e9a2a500b7dd65f67
rian <>
2022-01-01 17:19:51 +0700

    fix fragmentakun v1

--- app/src/main/java/com/example/reogorder/fragment/FragmentAkun.kt
+++ app/src/main/java/com/example/reogorder/fragment/FragmentAkun.kt
@@ -1,15 +1,24 @@
 package com.example.reogorder.fragment
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.reogorder.ActivityAbout
+import com.example.reogorder.ActivityWelcome
 import com.example.reogorder.R
 
 class FragmentAkun : Fragment() {
+    lateinit var alertDialog: AlertDialog.Builder
+    lateinit var SP: SharedPreferences
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_akun, container, false)
     }
@@ -38,5 +47,34 @@ class FragmentAkun : Fragment() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        SP = activity?.applicationContext!!.getSharedPreferences("Login", Context.MODE_PRIVATE)
+        alertDialog = AlertDialog.Builder(context)
+        val logout = activity?.findViewById<Button>(R.id.btnLogout)
+        logout!!.setOnClickListener {
+            alertDialog.setTitle("Logout")
+            alertDialog.setMessage("Apakah anda mau keluar dari akun ini ?")
+                .setCancelable(false)
+                .setPositiveButton("YA", object: DialogInterface.OnClickListener {
+                    override fun onClick(dialog: DialogInterface?, id: Int) {
+                        val editor = SP.edit()
+                        editor.putString("status","")
+                        editor.apply()
+
+                        val intent = Intent(context, ActivityWelcome::class.java)
+                        startActivity(intent)
+                    }
+                })
+
+                .setNegativeButton("TIDAK", object: DialogInterface.OnClickListener{
+                    override fun onClick(dialog: DialogInterface?, id: Int) {
+                        dialog?.cancel()
+                    }
+                }).create().show()
+        }
     }
 }
